1f0ffa4faa3f063d788377a4fc6ecf51683d6a51
Mariusz67 <putomariusz@gmail.com>
2022-04-09 18:56:33 +0200

    Wersja stabilna, bez dnia stolarza - wykomentowane

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -47,7 +47,7 @@ public abstract class Product {
         return price.multiply(taxPercent).add(price).add(exciseTax);
     }
 
-/*
+    /*
     public BigDecimal getPriceWithTaxWithExciseTaxOnCarpentersDay() {
         int cYear = 2022;
         int cMonth = 3;
@@ -59,5 +59,5 @@ public abstract class Product {
         }
         return price.multiply(taxPercent).add(price).add(exciseTax);
     }
-*/
+    */
 }
